7131a3f3f6c04cc5ae771eaa8e18bfd53115cce6
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-03-18 19:14:37 +0100

    fix(tests): fix prod pattern

--- .k8s/__tests__/prod.ts
+++ .k8s/__tests__/prod.ts
@@ -7,6 +7,6 @@ jest.setTimeout(1000 * 60);
 test("kosko generate --prod", async () => {
   process.env.HARBOR_PROJECT = "1000jours";
   expect(
-    await getEnvManifests("prod", "'!(_*)'", project("1000jours").prod)
+    await getEnvManifests("prod", "'!\\(_*\\)'", project("1000jours").prod)
   ).toMatchSnapshot();
 });
