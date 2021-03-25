629f0ee04f491ad38ec131b2d3e65c9c5c9b2451
Adrien Risser <risseraka@gmail.com>
2021-03-25 22:04:39 +0000

    fix: corrige l'index pour les fichiers images

--- back/hasura/migrations/1614770546376_1000jours/up.sql
+++ back/hasura/migrations/1614770546376_1000jours/up.sql
@@ -33,3 +33,9 @@ ALTER TABLE "public"."articles_thematiques__thematiques_articles"
     FOREIGN KEY ("thematique_id")
     REFERENCES "public"."thematiques"
     ("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "public"."upload_file_morph"
+    ADD CONSTRAINT "upload_file_morph_upload_file_id_fkey"
+    FOREIGN KEY ("upload_file_id")
+    REFERENCES "public"."upload_file"
+    ("id") ON UPDATE CASCADE ON DELETE CASCADE;
