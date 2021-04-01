93d712eee06b33bc6e6a73dcb41580dd83d28492
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-04-01 10:04:17 +0200

    fix(hasura): fix migrations (#89)

---
+++ back/hasura/migrations/1614770546377_1000jours/up.sql
@@ -0,0 +1,7 @@
+
+ALTER TABLE "public"."upload_file_morph" DROP CONSTRAINT IF EXISTS "upload_file_morph_upload_file_id_fkey";
+ALTER TABLE "public"."upload_file_morph"
+    ADD CONSTRAINT "upload_file_morph_upload_file_id_fkey"
+    FOREIGN KEY ("upload_file_id")
+    REFERENCES "public"."upload_file"
+    ("id") ON UPDATE CASCADE ON DELETE CASCADE;
