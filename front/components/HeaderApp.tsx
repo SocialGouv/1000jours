8e7dad0633f172cfae59cb56daf4be6c39bd13d6
Benjamin Guedj <benjaminguedj@hotmail.fr>
2021-04-12 12:20:43 +0200

    refactor: 💡 PR Corrige les retours UI (Onboarding et Profil)
    ✅ Closes: #121

--- front/components/HeaderApp.tsx
+++ front/components/HeaderApp.tsx
@@ -1,3 +1,4 @@
+import type { FC } from "react";
 import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
@@ -5,17 +6,15 @@ import LogoMinistere from "../assets/images/Logo ministere.svg";
 import Colors from "../constants/Colors";
 import Labels from "../constants/Labels";
 
-const HeaderApp: React.FC = () => {
-  return (
-    <View style={[styles.header]}>
-      <View style={[styles.logoMinistere]}>
-        <LogoMinistere height={32} />
-      </View>
-      <Text style={[styles.appLogo]}>{Labels.appName}</Text>
-      <View style={[styles.underline]}></View>
+const HeaderApp: FC = () => (
+  <View style={styles.header}>
+    <View style={styles.logoMinistere}>
+      <LogoMinistere height={32} />
     </View>
-  );
-};
+    <Text style={styles.appLogo}>{Labels.appName}</Text>
+    <View style={styles.underline}></View>
+  </View>
+);
 
 const styles = StyleSheet.create({
   appLogo: {
