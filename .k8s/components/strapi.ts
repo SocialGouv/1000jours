e58181bc4f726bad826b2d3d2ee4f4a43e50006e
Douglas Duteil <douglasduteil@gmail.com>
2021-06-27 12:10:48 +0200

    fix(k8s): use shared azure volume (#370)
    * build(k8s): use shared azure volume
    
    * chore(lock): update lock file
    
    * build(k8s): use external azureProjectVolume
    
    * chore(lock): update lock file
    
    * test: update snapshots
    
    * restore proxy-body-size annotation
    
    * test: update snapshots
    
    * fix(k8s): use new pg secret
    
    Co-authored-by: Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>

--- .k8s/components/strapi.ts
+++ .k8s/components/strapi.ts
@@ -1,119 +1,86 @@
 import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/app";
+import { azureProjectVolume } from "@socialgouv/kosko-charts/components/azure-storage/azureProjectVolume";
 import { addEnvs } from "@socialgouv/kosko-charts/utils/addEnvs";
-import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath";
 import { getIngressHost } from "@socialgouv/kosko-charts/utils/getIngressHost";
-import { getManifestByKind } from "@socialgouv/kosko-charts/utils/getManifestByKind";
-import { Deployment } from "kubernetes-models/apps/v1";
-import { PersistentVolumeClaim } from "kubernetes-models/v1";
+import { getDeployment } from "@socialgouv/kosko-charts/utils/getDeployment";
+import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath";
+import {
+  VolumeMount,
+  Probe,
+  ResourceRequirements,
+  Volume,
+} from "kubernetes-models/v1";
+import { Ingress } from "kubernetes-models/api/networking/v1";
 
-type configKey = "pvcName" | string;
+const component = "strapi";
+const params = env.component(component);
+const prob = new Probe({
+  httpGet: {
+    path: "/_health",
+    port: "http",
+  },
+  initialDelaySeconds: 30
+});
 
-type StrapiOptions = {
-  config?: Record<configKey, any>;
-  deployment?: Record<string, any>;
-};
+const resources = new ResourceRequirements({
+  requests: {
+    cpu: "50m",
+    memory: "256Mi",
+  },
+  limits: {
+    cpu: "500m",
+    memory: "1Gi",
+  },
+});
 
-const createStrapiComponent = async (
-  name: string,
-  options: StrapiOptions
-): Promise<{ kind: string }[]> => {
-  const manifests = [];
+export default async () => {
+  const name = "uploads";
+  const [persistentVolumeClaim, persistentVolume] = azureProjectVolume(name, {
+    storage: "5Gi",
+  });
+  const uploadsVolume = new Volume({
+    persistentVolumeClaim: { claimName: persistentVolumeClaim.metadata!.name! },
+    name,
+  });
+  const emptyDir = new Volume({ name, emptyDir: {} });
 
-  const config = {
-    containerPort: 1337,
-    withPostgres: true,
-    subDomainPrefix: "strapi-",
-    pvcName: "strapi-uploads",
-    ...(options.config || {}),
-  };
+  const uploadsVolumeMount = new VolumeMount({
+    mountPath: "/app/public/uploads",
+    name,
+  });
 
-  const deploymentConfig = {
-    ...(options.deployment || {}),
-    container: {
-      ...((options.deployment && options.deployment.container) || {}),
-      // override probes path
-      livenessProbe: {
-        httpGet: {
-          path: "/_health",
-          port: "http",
-        },
-      },
-      readinessProbe: {
-        httpGet: {
-          path: "/_health",
-          port: "http",
-        },
-      },
-      // increase startup delay
-      startupProbe: {
-        httpGet: {
-          path: "/_health",
-          port: "http",
-        },
-      },
-      resources: {
-        requests: {
-          cpu: "50m",
-          memory: "256Mi",
-        },
-        limits: {
-          cpu: "500m",
-          memory: "1Gi",
-        },
-      },
-      volumeMounts: [
-        {
-          mountPath: "/app/public/uploads",
-          name: "uploads",
-        },
-      ],
-    },
-  };
-
-  const strapiManifests = await create("strapi", {
+  // generate basic strapi manifests
+  const manifests = await create(component, {
     env,
-    config,
-    deployment: deploymentConfig,
-  });
-
-  const deployment = getManifestByKind(
-    strapiManifests,
-    //@ts-expect-error
-    Deployment
-  ) as Deployment;
-
-  const projectName = process.env.CI_PROJECT_NAME;
-  const pvcName = config.pvcName || `${projectName}-strapi-uploads`;
-
-  if (deployment && deployment?.spec?.template.spec) {
-    deployment.spec.template.spec.volumes = [
-      {
-        persistentVolumeClaim: {
-          claimName: pvcName,
-        },
-        name: "uploads",
+    config: {
+      withPostgres: true,
+      containerPort: 1337,
+      subDomainPrefix: "backoffice-",
+      image: getHarborImagePath({ name: "les1000jours-strapi" }),
+    },
+    deployment: {
+      container: {
+        livenessProbe: prob,
+        readinessProbe: prob,
+        startupProbe: prob,
+        resources,
+        volumeMounts: [uploadsVolumeMount],
       },
-    ];
+      volumes: [params.useEmptyDirAsVolume ? emptyDir : uploadsVolume],
+    },
+  });
+  const deployment = getDeployment(manifests);
+
+  // add nginx annotation for nginx upload limit
+  const ingress = manifests.find((m) => m.kind === "Ingress") as Ingress;
+  if (ingress && ingress.metadata?.annotations) {
+    ingress.metadata.annotations = {
+      ...ingress.metadata.annotations,
+      "nginx.ingress.kubernetes.io/proxy-body-size": "1g",
+    };
   }
-
-  const deploymentUrl = "https://" + getIngressHost(strapiManifests)
-
-  const pvc = new PersistentVolumeClaim({
-    metadata: {
-      name: pvcName,
-      annotations: {},
-    },
-    spec: {
-      accessModes: ["ReadWriteOnce"],
-      resources: {
-        requests: {
-          storage: "5Gi",
-        },
-      },
-      volumeMode: "Filesystem",
-    },
-  });
+  const deploymentUrl = "https://" + getIngressHost(manifests);
 
   addEnvs({
     deployment,
@@ -129,37 +96,7 @@ const createStrapiComponent = async (
     },
   });
 
-  manifests.push(...strapiManifests);
-  manifests.push(pvc);
-
-  return await manifests;
-};
-
-export default async () => {
-  // generate basic strapi manifests
-  const manifests = await createStrapiComponent("strapi", {
-    config: {
-      subDomainPrefix: "backoffice-",
-      image: getHarborImagePath({ name: "les1000jours-strapi" }),
-      pvcName: "1000jours-strapi-uploads" // persistant storage name
-    },
-  });
-
-  // add nginx annotation for nginx upload limit
-  const ingress = manifests.find(m=>m.kind==="Ingress") as Deployment;
-  if (ingress && ingress.metadata?.annotations) {
-    ingress.metadata.annotations = {
-      ...ingress.metadata.annotations,
-      "nginx.ingress.kubernetes.io/proxy-body-size": "1g"
-    }
-  }
-
-  // add a nodeSelector to improve volume/deploy performance  // TODO
-  const deployment = manifests.find(m=>m.kind==="Deployment") as Deployment;
-  if (deployment && deployment?.spec?.template.spec) {
-    deployment.spec.template.spec.nodeSelector = {
-      workload: "1000jours-strapi",
-    };
-  }
-  return manifests;
+  return manifests.concat(
+    params.useEmptyDirAsVolume ? [] : [persistentVolumeClaim, persistentVolume]
+  );
 };
