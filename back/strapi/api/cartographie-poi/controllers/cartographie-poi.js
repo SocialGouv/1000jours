345c7a4651880fb0b094742e22cbb7c5c01838e6
Adrien Risser <risseraka@gmail.com>
2021-07-12 18:16:32 +0200

    feat(carto): Ajoute une requête de comptage de POIs

--- back/strapi/api/cartographie-poi/controllers/cartographie-poi.js
+++ back/strapi/api/cartographie-poi/controllers/cartographie-poi.js
@@ -4,20 +4,29 @@ const PoiService = require("../services");
 
 const params = ["perimetre", "types", "thematiques", "etapes"];
 
+const buildParams = (context) =>
+  params.reduce((result, param) => {
+    const value = context.request.query[param];
+
+    result[param] = value ? JSON.parse(value) : null;
+
+    return result;
+  }, {});
+
 const search = async (context) => {
   try {
-    return PoiService.search(
-      params.reduce((result, param) => {
-        const value = context.request.query[param];
-
-        result[param] = value ? JSON.parse(value) : null;
-
-        return result;
-      }, {})
-    );
+    return PoiService.search(buildParams(context));
   } catch (e) {
     context.badRequest(e.message);
   }
 };
 
-module.exports = { search };
+const count = async (context) => {
+  try {
+    return PoiService.count(buildParams(context));
+  } catch (e) {
+    context.badRequest(e.message);
+  }
+};
+
+module.exports = { search, count };
