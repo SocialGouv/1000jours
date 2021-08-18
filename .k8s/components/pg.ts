c536392801b65d2d79d8a6b20893348dc33f1007
Douglas DUTEIL <douglasduteil@gmail.com>
2021-08-18 15:09:42 +0200

    build(k8s): update to @socialgouv/kosko-charts 9.2

--- .k8s/components/pg.ts
+++ .k8s/components/pg.ts
@@ -1,34 +1,6 @@
 import env from "@kosko/env";
-import { SealedSecret } from "@kubernetes-models/sealed-secrets/bitnami.com/v1alpha1/SealedSecret";
-
-import { loadYaml } from "@socialgouv/kosko-charts/utils/getEnvironmentComponent";
-import { updateMetadata } from "@socialgouv/kosko-charts/utils/updateMetadata";
-import ci from "@socialgouv/kosko-charts/environments";
-
 import { create } from "@socialgouv/kosko-charts/components/azure-pg";
 
-export default async () => {
-  if (env.env === "dev") {
-    return create({
-      env,
-    });
-  }
-
-  // in prod/preprod, we try to add a fixed sealed-secret
-  const secret = await loadYaml<SealedSecret>(
-    env,
-    `pg-user.sealed-secret.yaml`
-  );
-  if (!secret) {
-    return [];
-  }
-
-  const envParams = ci(process.env);
-  // add gitlab annotations
-  updateMetadata(secret, {
-    annotations: envParams.metadata.annotations || {},
-    labels: envParams.metadata.labels || {},
-    namespace: envParams.metadata.namespace,
-  });
-  return [secret];
-};
+export default create("pg-user", {
+  env,
+});
