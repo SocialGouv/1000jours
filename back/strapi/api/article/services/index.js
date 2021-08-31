cc586c873412b9c855486ba9f8ddcc177dda2610
Adrien Risser <risseraka@gmail.com>
2021-08-31 16:26:13 +0200

    feat(parentheque): Ajoute le modèle de documents dans le backoffice, refs #626

--- back/strapi/api/article/services/index.js
+++ back/strapi/api/article/services/index.js
@@ -1,51 +1,14 @@
 "use strict";
 
-const { sanitizeEntity } = require("strapi-utils");
+const ModelsService = require("../../models/services");
 
-const recurseWalk = (obj, func) =>
-  Object.keys(obj).reduce((obj, key) => {
-    if (obj[key] && typeof obj[key] === "object") {
-      recurseWalk(obj[key], func);
-    } else {
-      func(obj[key], key, obj);
-    }
-
-    return obj;
-  }, obj);
-
-const completeUrls = (article) => {
-  const serverUrl = strapi.config.get("server.url");
-
-  return recurseWalk(article, (value, key, obj) => {
-    if (typeof value !== "string") return;
-
-    obj[key] = value.replace(/(^|")\/uploads/g, `$1${serverUrl}/uploads`);
-  });
-};
-
-// sanitize first-level text fields
-const sanitizeTexts = (data) =>
-  Object.keys(data).reduce((data, key) => {
-    if (typeof data[key] === "string") {
-      data[key] = data[key].trim().replace(/(\s)+/g, "$1");
-    }
-
-    return data;
-  }, data);
-
-const format = (article) => {
+const format = (entity) => {
   // fixes weird empty object issue and GraphQL `cannot return null` error
-  if (article.visuel && !article.visuel.url) {
-    article.visuel.url = "";
+  if (entity.visuel && !entity.visuel.url) {
+    entity.visuel.url = "";
   }
 
-  return completeUrls(
-    sanitizeEntity(article, { model: strapi.models.article })
-  );
+  return ModelsService.format(entity, "article");
 };
 
-module.exports = {
-  completeUrls,
-  format,
-  sanitizeTexts,
-};
+module.exports = { ...ModelsService, format };
