01fc7b77435fb2f6b0cfbc31e44fce3cc3c1e5bd
Yannick JACQUELINE <yjacqueline@ippon.fr>
2021-10-04 16:03:42 +0200

    fix(epds): réajout de la variable RESULT_BECONTACTED_VALUE

--- front/constants/epdsConstants.constants.ts
+++ front/constants/epdsConstants.constants.ts
@@ -8,6 +8,7 @@ export enum EpdsGender {
 export const RESULT_WELL_VALUE = 9;
 export const RESULT_NOTSOWELL_VALUE = 12;
 export const RESULT_BAD_VALUE = 13;
+export const RESULT_BECONTACTED_VALUE = 15;
 
 export enum ResultIconValueEnum {
   bien = "bien",
