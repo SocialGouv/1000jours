15525d90e8fff3545a7f01c3a9a4f441acdb8ff8
Yannick JACQUELINE <yjacqueline@ippon.fr>
2021-10-05 17:39:57 +0200

    fix(carto): ajout picker pour Android

--- front/components/base/customNumberOfChildrenPicker.component.tsx
+++ front/components/base/customNumberOfChildrenPicker.component.tsx
@@ -1,10 +1,11 @@
-import { PickerIOS } from "@react-native-community/picker";
+import { Picker, PickerIOS } from "@react-native-community/picker";
 import { range } from "lodash";
 import * as React from "react";
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 
 import { Colors, FontWeight, Labels, Margins } from "../../constants";
+import { PLATFORM_IS_IOS } from "../../constants/platform.constants";
 import { CommonText } from "..";
 
 interface Props {
@@ -14,7 +15,7 @@ interface Props {
 const CustomNumberOfChildrenPicker: React.FC<Props> = ({
   updateNumberOfChildren,
 }) => {
-  const INITIAL_NUMBER_OF_CHILDREN = 0;
+  const INITIAL_NUMBER_OF_CHILDREN = 1;
   const MAX_NUMBER_OF_CHILDREN = 4;
   const [numberOfChildren, setNumberOfChildren] = useState(
     INITIAL_NUMBER_OF_CHILDREN
@@ -25,27 +26,44 @@ const CustomNumberOfChildrenPicker: React.FC<Props> = ({
       <CommonText style={styles.textStyle}>
         {Labels.epdsSurvey.beContacted.numberOfChildren}
       </CommonText>
-      <PickerIOS
-        selectedValue={numberOfChildren}
-        style={styles.pickerStyle}
-        onValueChange={(itemValue) => {
-          setNumberOfChildren(Number(itemValue));
-          updateNumberOfChildren(Number(itemValue));
-        }}
-      >
-        {range(INITIAL_NUMBER_OF_CHILDREN, MAX_NUMBER_OF_CHILDREN).map(
-          (value) => (
-            <PickerIOS.Item key={value} label={String(value)} value={value} />
-          )
-        )}
-      </PickerIOS>
+      {PLATFORM_IS_IOS ? (
+        <PickerIOS
+          selectedValue={numberOfChildren}
+          style={styles.pickerStyle}
+          onValueChange={(itemValue) => {
+            setNumberOfChildren(Number(itemValue));
+            updateNumberOfChildren(Number(itemValue));
+          }}
+        >
+          {range(INITIAL_NUMBER_OF_CHILDREN, MAX_NUMBER_OF_CHILDREN).map(
+            (value) => (
+              <PickerIOS.Item key={value} label={String(value)} value={value} />
+            )
+          )}
+        </PickerIOS>
+      ) : (
+        <Picker
+          selectedValue={numberOfChildren}
+          style={styles.pickerStyle}
+          onValueChange={(itemValue) => {
+            setNumberOfChildren(Number(itemValue));
+            updateNumberOfChildren(Number(itemValue));
+          }}
+        >
+          {range(INITIAL_NUMBER_OF_CHILDREN, MAX_NUMBER_OF_CHILDREN).map(
+            (value) => (
+              <Picker.Item key={value} label={String(value)} value={value} />
+            )
+          )}
+        </Picker>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   pickerStyle: {
-    width: 60,
+    width: 100,
   },
   rowView: {
     flexDirection: "row",
