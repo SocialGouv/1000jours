f2cadc04ffe412c99c10819a0e90c5d516a4d605
yannickjacqueline <82370429+yannickjacqueline@users.noreply.github.com>
2021-10-06 16:41:06 +0200

    feat: intégration de la lib métier (#750)

---
+++ lib/README.md
@@ -0,0 +1,2 @@
+# Nos 1000 jours Lib package
+Cette librairie est utilisée pour regrouper le code métier du projet 1000 jours, et est utilisée par l'application mobile ainsi que le site web pro.
---
+++ lib/src/models/poi/constants.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export enum PoiTypeEnum {
+  planning_familial = "planning_familial",
+  maison_de_naissance = "maison_de_naissance",
+  maternite = "maternite",
+  pmi = "pmi", // pas d'icône
+  saad = "saad",
+  cpam = "cpam", // pas d'icône
+  caf = "caf", // pas d'icône
+  mairie = "mairie", // pas d'icône
+}
+
+export enum PoiCategorieEnum {
+  structure = "structure",
+  professionnel = "professionnel",
+}
