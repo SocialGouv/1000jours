1c25c9a4527ad8f9112ed19eac7bac42c8332498
Yannick JACQUELINE <yjacqueline@ippon.fr>
2021-10-07 15:53:06 +0200

    feat: intégration de la lib métier (#750)

---
+++ lib/src/models/epds/epds.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export interface EpdsSurvey {
+  id: number;
+  locale: string;
+  ordre: number;
+  libelle: string;
+  reponse_1_libelle: string;
+  reponse_1_points: number;
+  reponse_2_libelle: string;
+  reponse_2_points: number;
+  reponse_3_libelle: string;
+  reponse_3_points: number;
+  reponse_4_libelle: string;
+  reponse_4_points: number;
+}
