f663910a2421a4ba326095c18c0cb3f0ad2ec5c1
yannickjacqueline <82370429+yannickjacqueline@users.noreply.github.com>
2021-10-28 15:55:46 +0200

    fix(epds): corrections retours PO (#822)

--- front/constants/Colors.ts
+++ front/constants/Colors.ts
@@ -31,6 +31,8 @@ export default {
   secondaryGreen: "#83BCA0",
   secondaryGreenDark: "#5A8C74",
   secondaryGreenLight: "#F0FFEF",
+  secondaryRedDark: "#FFEDDF",
+  secondaryRedMiddle: "#D15903",
   secondaryRedLight: "#E86404",
   navigation: "#DCDCDC",
   commonText: "#4C4C4C",
