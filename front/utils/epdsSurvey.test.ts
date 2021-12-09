5a3c4d75989775903e4f75ecd8b8b6c68898b050
Audrey Lebret <alebret@ippon.fr>
2021-12-09 14:22:32 +0100

    fix(epde): activer la notif de rapelle que le score de la Q10 est 3

--- front/utils/epdsSurvey.test.ts
+++ front/utils/epdsSurvey.test.ts
@@ -1,15 +1,23 @@
 import * as EpdsSurveyUtils from "./epdsSurvey.util";
 
 describe("EPDS Survey utils", () => {
-  it("Score is less than 11", () => {
-    expect(EpdsSurveyUtils.showContactReminder(5)).toBe(false);
+  it("Score is less than 11 & question 10 score is 0", () => {
+    expect(EpdsSurveyUtils.showContactReminder(5, 0)).toBe(false);
   });
 
-  it("Score is 11", () => {
-    expect(EpdsSurveyUtils.showContactReminder(11)).toBe(false);
+  it("Score is less than 11 & question 10 score is 3", () => {
+    expect(EpdsSurveyUtils.showContactReminder(5, 3)).toBe(true);
   });
 
-  it("Score is greater than 11", () => {
-    expect(EpdsSurveyUtils.showContactReminder(14)).toBe(true);
+  it("Score is 11 & question 10 score is 0", () => {
+    expect(EpdsSurveyUtils.showContactReminder(11, 0)).toBe(false);
+  });
+
+  it("Score is greater than 11 & question 10 score is 0", () => {
+    expect(EpdsSurveyUtils.showContactReminder(14, 0)).toBe(true);
+  });
+
+  it("Score is greater than 11 & question 10 score is 3", () => {
+    expect(EpdsSurveyUtils.showContactReminder(14, 3)).toBe(true);
   });
 });
