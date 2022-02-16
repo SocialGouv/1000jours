aae0185ae8a4ad2467a78fd4af3d48f9a5371ad8
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-02-16 12:52:56 +0100

    fix(k8s): use pg-user

--- .k8s/components/pg.ts
+++
@@ -1,9 +0,0 @@
-import env from "@kosko/env";
-import { create } from "@socialgouv/kosko-charts/components/azure-pg";
-
-export default create("pg-user", {
-  env,
-  config: {
-    pgHost: "les1000joursdevserver.postgres.database.azure.com"
-  }
-});
