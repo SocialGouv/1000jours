ddef44b558a67f31a840797f8d01086fb28ceea2
Adrien Risser <risseraka@gmail.com>
2022-03-10 18:01:14 +0100

    feat(articles): Permet la recherche avec ou sans diacritique (#1109)

--- back/strapi/api/article/services/index.js
+++ back/strapi/api/article/services/index.js
@@ -1,6 +1,11 @@
 "use strict";
 
+const ArticleModel = require("../models/article.settings.js");
 const ModelsService = require("../../models/services");
+
+const normalizeString = (str) => str
+  ? str.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()
+  : "";
 
 const format = (entity) => {
   // fixes weird empty object issue and GraphQL `cannot return null` error
@@ -11,4 +16,29 @@ const format = (entity) => {
   return ModelsService.format(entity, "article");
 };
 
-module.exports = { ...ModelsService, format };
+const ArticleTextFields = Object.keys(ArticleModel.attributes).filter(field =>
+  ["text", "richtext", "string"].indexOf(ArticleModel.attributes[field].type) !== -1
+);
+
+const search = async ({ query }) => {
+  const knex = strapi.connections.default;
+
+  query = normalizeString(query);
+
+  const articles = await strapi.query("article")
+  .find({ published_at_null: false });
+
+  if (!articles.length) return [];
+
+  return articles.filter(article =>
+    ArticleTextFields.some(field =>
+      normalizeString(article[field]).match(query)
+    )
+  );
+};
+
+module.exports = {
+  ...ModelsService,
+  search,
+  format,
+};
