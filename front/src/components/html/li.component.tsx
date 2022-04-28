8df8a63c260623d61172edfed434ec94bee8947e
benguedj <71835422+benguedj@users.noreply.github.com>
2022-04-28 16:48:23 +0200

    fix: 🐛 Corrige les problèmes d'accessibilité (puces et tableau) (#1184)
    ✅ Closes: #1150
    
    Co-authored-by: Benjamin Guedj <benjaminguedj@hotmail.fr>

--- front/src/components/html/li.component.tsx
+++ front/src/components/html/li.component.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { StyleSheet } from "react-native";
 
-import SpecialChars from "../../constants/specialChars";
 import { Paddings } from "../../styles";
 import type { TextProps } from "../baseComponents";
 import { SecondaryText } from "../baseComponents";
@@ -9,7 +8,7 @@ import { SecondaryText } from "../baseComponents";
 const Li: React.FC<TextProps> = (props) => {
   const newProps = {
     ...props,
-    children: `${SpecialChars.bullet} ${props.children}`,
+    children: `${props.children}`,
   };
   return <SecondaryText {...newProps} style={[styles.li, props.style]} />;
 };
