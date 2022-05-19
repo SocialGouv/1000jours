97d8d1ab05cfd6f4792df8dd82d1d43930a448c8
Benjamin Guedj <bguedj.freelance@gmail.com>
2022-05-19 18:19:18 +0200

    fix: supprime l'enregistrement du token pour les notification push (non utilisé)

--- front/src/components/notification/notificationHandler.component.tsx
+++ front/src/components/notification/notificationHandler.component.tsx
@@ -38,7 +38,6 @@ const NotificationHandler: FC = () => {
 
   useEffect(() => {
     // Notifications
-    void NotificationUtils.registerForPushNotificationsAsync();
     // Se déclenche lorsque l'on reçoit une notification et que l'app est ouverte
     notificationListener.current =
       Notifications.addNotificationReceivedListener((newNotification) => {
