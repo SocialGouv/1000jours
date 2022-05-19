b9af0ff8ba4dc300bd004d4a44cf0a1b80dc7c2f
Benjamin Guedj <bguedj.freelance@gmail.com>
2022-05-19 11:35:16 +0200

    fix: remove @unimodules/core (expo-notifications)

--- front/src/components/notification/notificationHandler.component.tsx
+++ front/src/components/notification/notificationHandler.component.tsx
@@ -1,9 +1,9 @@
-import type { Subscription } from "@unimodules/core";
 import type { Notification } from "expo-notifications";
 import * as Notifications from "expo-notifications";
 import type { FC } from "react";
 import { useCallback, useEffect, useRef, useState } from "react";
 import * as React from "react";
+import { Subscription } from 'expo-modules-core';
 
 import { Labels } from "../../constants";
 import { NotificationUtils, TrackerUtils } from "../../utils";
