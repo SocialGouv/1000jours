b1ba0e356d592e9bd4a606a2ed453ce65eb5d4dd
Audrey Lebret <alebret@ippon.fr>
2022-06-21 16:57:08 +0200

    feat(widget): envoi des mails pour les intentions (#1257)
    * feat(widget): envoi des mails pour les intentions sans pdf
    
    * feat(widget): MAJ des mails pour les intentions
    
    * feat(widget): maj mail intentions avec pdf
    
    * fix(widget): suppr d'un todo
    
    * fix(widget): maj des mails
    
    * fix(widget): retours PR
    
    * fix(widget): maj wording

--- back/strapi/api/reponses-epds/controllers/reponses-epds.js
+++ back/strapi/api/reponses-epds/controllers/reponses-epds.js
@@ -1,9 +1,13 @@
 const contact = async () => { };
 const partage = async () => { };
 const createReponsesEpdsWidget = async () => { };
+const partagePourSoiMeme = async () => { };
+const partageEntourage = async () => { };
 
 module.exports = {
   contact,
+  createReponsesEpdsWidget,
   partage,
-  createReponsesEpdsWidget
+  partageEntourage,
+  partagePourSoiMeme,
 };
