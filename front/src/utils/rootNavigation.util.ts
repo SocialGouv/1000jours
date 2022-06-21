b8156b6add18b84bfc9f2b70b987d1a6ea52c0e3
Benjamin Guedj <bguedj.freelance@gmail.com>
2022-06-21 12:04:59 +0200

    feat: 🎸 Trace une erreur lorsque 'navigationRef is not ready'

--- front/src/utils/rootNavigation.util.ts
+++ front/src/utils/rootNavigation.util.ts
@@ -1,5 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { createNavigationContainerRef } from "@react-navigation/native";
+import { Native } from "sentry-expo";
+
+const WAITING_TIME = 250;
+const MAX_WAITING_TIME = 4000;
 
 export const navigationRef = createNavigationContainerRef();
 
@@ -9,44 +13,52 @@ export const navigate = async (
   stayOnCurrentTab?: boolean
 ): Promise<void> => {
   let stop = false;
+  setTimeout(function () {
+    stop = true;
+  }, MAX_WAITING_TIME);
   while (!stop) {
     if (navigationRef.isReady()) {
       stop = true;
     } else {
-      await new Promise((resolve) => setTimeout(resolve, 250));
+      await new Promise((resolve) => setTimeout(resolve, WAITING_TIME));
     }
   }
 
-  // Information : Pour naviguer vers une page depuis n'importe où
-  // Il faut passer par les screens du Stack.Navigator, ensuite passer par les BottomTab.Navigator puis par les BottomTab.Screen.
-  // Ex : root > tabCalendar > tabCalendarScreen
-  let screen = name;
-  if (!stayOnCurrentTab) {
-    switch (name) {
-      case "article":
-        screen = "root";
-        params = {
-          params: {
-            params: params,
-            screen: "article",
-          },
-          screen: "tabHome",
-        };
-        break;
-      case "event":
-        screen = "root";
-        params = {
-          params: {
-            params: params,
-            screen: "tabCalendarScreen",
-          },
-          screen: "tabCalendar",
-        };
-        break;
-      default:
-        break;
+  if (navigationRef.isReady()) {
+    // Information : Pour naviguer vers une page depuis n'importe où
+    // Il faut passer par les screens du Stack.Navigator, ensuite passer par les BottomTab.Navigator puis par les BottomTab.Screen.
+    // Ex : root > tabCalendar > tabCalendarScreen
+    let screen = name;
+    if (!stayOnCurrentTab) {
+      switch (name) {
+        case "article":
+          screen = "root";
+          params = {
+            params: {
+              params: params,
+              screen: "article",
+            },
+            screen: "tabHome",
+          };
+          break;
+        case "event":
+          screen = "root";
+          params = {
+            params: {
+              params: params,
+              screen: "tabCalendarScreen",
+            },
+            screen: "tabCalendar",
+          };
+          break;
+        default:
+          break;
+      }
     }
-  }
 
-  navigationRef.navigate(screen as never, params as never);
+    navigationRef.navigate(screen as never, params as never);
+  } else {
+    const errorMessage = `Error from rootNavigation.utils : 'navigationRef is not ready after waiting ${MAX_WAITING_TIME}ms'`;
+    Native.captureException(errorMessage);
+  }
 };
