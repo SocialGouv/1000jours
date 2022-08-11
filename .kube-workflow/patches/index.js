dd16295f5e3d620ffb6874184abbf91f2b304cbf
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-08-11 11:24:52 +0200

    fix

--- .kube-workflow/patches/index.js
+++ .kube-workflow/patches/index.js
@@ -1,5 +1,6 @@
 module.exports = async (manifests) => {
   const deploy = manifests.find((manifest) => manifest.kind === "Deployment");
+  // force readinessProbe.initialDelaySeconds
   deploy.spec.template.spec.containers =
     deploy.spec.template.spec.containers.map((container) => ({
       ...container,
