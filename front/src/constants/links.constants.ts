cec0c99f0f562fa1beee0bf0349848c7e02a857a
Benjamin Guedj <bguedj.freelance@gmail.com>
2022-09-01 12:05:06 +0200

    fix: 🐛 Corrige les urls de l'app vers les stores

--- front/src/constants/links.constants.ts
+++ front/src/constants/links.constants.ts
@@ -1,5 +1,6 @@
 export const reviewTypeForm = "https://startupdetat.typeform.com/to/kdw7bOcv";
 export const deepLinkUrl = `https://${process.env.DEEPLINK_DOMAIN}/${process.env.DEEPLINK_PATH}`;
-export const appUrlAndroid = "market://details?id=com.fabrique.millejours";
+export const appUrlAndroid =
+  "https://play.google.com/store/apps/details?id=com.fabrique.millejours";
 export const appUrliOS =
-  "itms-apps://itunes.com/apps/1000-premiers-jours/id1573729958";
+  "itms-apps://itunes.com/app/1000-premiers-jours/id1573729958";
