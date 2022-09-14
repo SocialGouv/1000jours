beda804abca8d23212e8b8882f9a9f2913a8cfa0
Benjamin Guedj <bguedj.freelance@gmail.com>
2022-09-14 16:31:13 +0200

    refactor: 💡 PR  Les 'Toogles' sont activés par défaut (notifs)

--- front/src/utils/notifications/notificationToggle.util.test.ts
+++ front/src/utils/notifications/notificationToggle.util.test.ts
@@ -36,7 +36,7 @@ describe("Notification Toggle Utils", () => {
       );
     });
 
-    it("should return false when key for type is undefined (toogle default value)", async () => {
+    it("should return true when key for type is undefined (toogle default value)", async () => {
       await NotificationToggleUtils.isToggleOn(NotificationType.epds).then(
         (data) => {
           expect(data).toEqual(true);
