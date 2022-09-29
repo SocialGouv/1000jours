044186e26cab0e9d9f07130ccb2a9085e43a9c3c
Lebret Audrey <alebret@ippon.fr>
2022-09-29 11:06:56 +0200

    fix(accessibility): puce politique de conf

--- front/src/components/html/li.component.tsx
+++ front/src/components/html/li.component.tsx
@@ -1,19 +1,38 @@
 import * as React from "react";
 import { StyleSheet } from "react-native";
 
-import { Paddings } from "../../styles";
+import SpecialChars from "../../constants/specialChars";
+import { Colors, Margins, Paddings, Sizes } from "../../styles";
 import type { TextProps } from "../baseComponents";
-import { SecondaryText } from "../baseComponents";
+import { SecondaryText, View } from "../baseComponents";
 
 const Li: React.FC<TextProps> = (props) => {
   const newProps = {
     ...props,
     children: `${props.children}`,
   };
-  return <SecondaryText {...newProps} style={[styles.li, props.style]} />;
+  return (
+    <View style={styles.content}>
+      <SecondaryText style={styles.dot} accessibilityLabel="Puces">
+        {SpecialChars.blackLargeCircle}
+      </SecondaryText>
+      <SecondaryText {...newProps} style={[styles.li, props.style]} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
+  content: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  dot: {
+    color: Colors.primaryBlue,
+    fontSize: Sizes.xxxxs,
+    lineHeight: Sizes.lg,
+    marginRight: Margins.default,
+    textAlignVertical: "top",
+  },
   li: {
     paddingVertical: Paddings.smallest,
   },
