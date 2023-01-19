845a087998da48b08cec73c9977f110b839db3c9
Lebret Audrey <alebret@ippon.fr>
2023-01-19 17:51:27 +0100

    fix(epds): refacto

--- front/src/screens/epdsSurvey/tabEpdsScreen.component.tsx
+++ front/src/screens/epdsSurvey/tabEpdsScreen.component.tsx
@@ -1,59 +1,16 @@
 import type { FC } from "react";
-import { useCallback, useEffect, useState } from "react";
 import * as React from "react";
 import { StyleSheet } from "react-native";
 import WebView from "react-native-webview";
 
-import {
-  EpdsGenderEntry,
-  EpdsOnboarding,
-  EpdsSurveyContent,
-} from "../../components";
 import { View } from "../../components/baseComponents";
 import TrackerHandler from "../../components/tracker/trackerHandler.component";
-import {
-  EpdsDbQueries,
-  FetchPoliciesConstants,
-  StorageKeysConstants,
-} from "../../constants";
-import { useAccessibilityReader } from "../../hooks";
-import { GraphQLQuery } from "../../services";
-import type { EpdsQuestionAndAnswers } from "../../type";
-import { EpdsSurveyUtils, StorageUtils, TrackerUtils } from "../../utils";
+import { TrackerUtils } from "../../utils";
 
-// TODO: refacto & clear
 const EPDS_WIDGET_SOURCE = "1000j-application";
 
+// TODO: refacto & clear
 const TabEpdsScreen: FC = () => {
-  const [isOnboardingDone, setIsOnboardingDone] = useState(false);
-  const [isGenderEntered, setIsGenderEntered] = useState(false);
-  const [questionAndAnswers, setQuestionAndAnswers] = useState<
-    EpdsQuestionAndAnswers[]
-  >([]);
-  const isAccessibilityModeOn = useAccessibilityReader();
-
-  useEffect(() => {
-    const getGenderFromStorage = async () => {
-      const genderValue = await StorageUtils.getStringValue(
-        StorageKeysConstants.epdsGenderKey
-      );
-      setIsGenderEntered(Boolean(genderValue));
-    };
-    void getGenderFromStorage();
-  }, []);
-
-  const handleResults = useCallback((data: unknown) => {
-    setQuestionAndAnswers(EpdsSurveyUtils.getQuestionsAndAnswersFromData(data));
-  }, []);
-
-  const goToEpdsSurvey = useCallback(() => {
-    setIsGenderEntered(true);
-  }, []);
-
-  const onBoardingIsDone = useCallback(() => {
-    setIsOnboardingDone(true);
-  }, []);
-
   const getViewToDisplay = () => {
     // TODO: urilisation de la branche de dev du widget pour les tests
     // uri à utiliser `${process.env.EPDS_WIDGET_URL}/?source=${EPDS_WIDGET_SOURCE}`
@@ -73,11 +30,6 @@ const TabEpdsScreen: FC = () => {
   return (
     <View style={styles.mainContainer}>
       <TrackerHandler screenName={TrackerUtils.TrackingEvent.EPDS} />
-      <GraphQLQuery
-        query={EpdsDbQueries.EPDS_SURVEY}
-        fetchPolicy={FetchPoliciesConstants.NO_CACHE}
-        getFetchedData={handleResults}
-      />
       {getViewToDisplay()}
     </View>
   );
