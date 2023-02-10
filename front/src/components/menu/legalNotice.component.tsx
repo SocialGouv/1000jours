a9ee6948d850c9a30ff4617522f3b5d4f735e439
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-02-10 09:47:47 +0100

    Update legalNotice.component.tsx

--- front/src/components/menu/legalNotice.component.tsx
+++ front/src/components/menu/legalNotice.component.tsx
@@ -55,7 +55,7 @@ const LegalNotice: FC = () => {
         Pour en savoir plus sur la politique d’accessibilité numérique de l’État
         :
       </P>
-      <A url="http://references.modernisation.gouv.fr/accessibilite-numerique">
+      <A url="https://references.modernisation.gouv.fr/accessibilite-numerique">
         Référentiel général d'amélioration de l'accessibilité
       </A>
 
