d85c40356ce853d1bad3ae3ec2af378323d19547
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-02-10 09:47:13 +0100

    fix: use https

--- front/src/components/menu/accessibility.component.tsx
+++ front/src/components/menu/accessibility.component.tsx
@@ -92,7 +92,7 @@ const Accessibility: FC = () => {
         Pour en savoir plus sur la politique d’accessibilité numérique de l’État
         :
       </P>
-      <A url="http://references.modernisation.gouv.fr/accessibilite-numerique">
+      <A url="https://references.modernisation.gouv.fr/accessibilite-numerique">
         Référentiel général d'amélioration de l'accessibilité
       </A>
     </ScrollView>
