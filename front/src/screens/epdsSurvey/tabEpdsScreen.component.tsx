27af76ff521300c776b801426870f540e6c16326
Lebret Audrey <alebret@ippon.fr>
2023-02-16 18:20:15 +0100

    feat(widget): ajout partenaires

--- front/src/screens/epdsSurvey/tabEpdsScreen.component.tsx
+++ front/src/screens/epdsSurvey/tabEpdsScreen.component.tsx
@@ -1,9 +1,10 @@
 import type { FC } from "react";
 import { useCallback } from "react";
 import * as React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Image } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 
+import { PartnersAssets } from "../../components/assets";
 import {
   CommonText,
   CustomButton,
@@ -11,8 +12,24 @@ import {
 } from "../../components/baseComponents";
 import TrackerHandler from "../../components/tracker/trackerHandler.component";
 import { Labels } from "../../constants";
-import { Margins } from "../../styles";
+import { Margins, Sizes } from "../../styles";
 import { LinkingUtils, TrackerUtils } from "../../utils";
+
+import MilleJours from "../../assets/images/partners/1000-premiers-jours.png";
+import BibaMagazine from "../../assets/images/partners/bibamagazine.jpg";
+import Bussy from "../../assets/images/partners/bussy.png";
+import Cnaf from "../../assets/images/partners/cnaf.png";
+import Doctissimo from "../../assets/images/partners/doctissimo.jpeg";
+import EnjoyFamily from "../../assets/images/partners/enjoy-family.jpeg";
+import MamanBlues from "../../assets/images/partners/maman-blues.jpeg";
+import MonenfantFr from "../../assets/images/partners/monenfant-fr.png";
+import Mpedia from "../../assets/images/partners/mpedia.jpeg";
+import Orehane from "../../assets/images/partners/orehane.jpeg";
+import Parents from "../../assets/images/partners/parents.png";
+import Path from "../../assets/images/partners/path.png";
+import ProsPetiteEnfance from "../../assets/images/partners/pros-petite-enfance.png";
+import Spf from "../../assets/images/partners/spf.jpeg";
+import Wemoms from "../../assets/images/partners/wemoms.png";
 
 const TabEpdsScreen: FC = () => {
   const onOpenWidget = useCallback(() => {
@@ -30,6 +47,12 @@ const TabEpdsScreen: FC = () => {
         <CommonText style={styles.textDescription}>
           {Labels.epdsSurvey.epdsPresentation.description}
         </CommonText>
+        <View style={styles.logoBlues}>
+          <PartnersAssets.MillesJoursBlues
+            width={Sizes.xxxxxxl}
+            height={Sizes.xxxxxxl}
+          />
+        </View>
         <CustomButton
           title={Labels.epdsSurvey.epdsPresentation.button}
           rounded
@@ -43,10 +66,31 @@ const TabEpdsScreen: FC = () => {
           rounded
           action={onOpenWidget}
         />
+        {partnersBloc()}
       </View>
     );
   };
-
+  const partnersBloc = () => {
+    return (
+      <View style={styles.logosPartners}>
+        <Image source={MilleJours} style={styles.logoImage} />
+        <Image source={BibaMagazine} style={styles.logoImage} />
+        <Image source={Bussy} style={styles.logoImage} />
+        <Image source={Cnaf} style={styles.logoImage} />
+        <Image source={Doctissimo} style={styles.logoImage} />
+        <Image source={EnjoyFamily} style={styles.logoImage} />
+        <Image source={MamanBlues} style={styles.logoImage} />
+        <Image source={MonenfantFr} style={styles.logoImage} />
+        <Image source={Mpedia} style={styles.logoImage} />
+        <Image source={Orehane} style={styles.logoImage} />
+        <Image source={Parents} style={styles.logoImage} />
+        <Image source={Path} style={styles.logoImage} />
+        <Image source={ProsPetiteEnfance} style={styles.logoImage} />
+        <Image source={Spf} style={styles.logoImage} />
+        <Image source={Wemoms} style={styles.logoImage} />
+      </View>
+    );
+  };
   return (
     <ScrollView style={styles.mainContainer}>
       <TrackerHandler screenName={TrackerUtils.TrackingEvent.EPDS} />
@@ -56,6 +100,23 @@ const TabEpdsScreen: FC = () => {
 };
 
 const styles = StyleSheet.create({
+  logoBlues: {
+    alignSelf: "center",
+    backgroundColor: "transparent",
+    marginBottom: Margins.default,
+  },
+  logoImage: {
+    height: Sizes.xxxxxxxl,
+    resizeMode: "contain",
+    width: Sizes.xxxxxxxl,
+  },
+  logosPartners: {
+    alignSelf: "center",
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginVertical: Margins.default,
+  },
   mainContainer: {
     flex: 1,
     marginHorizontal: Margins.larger,
