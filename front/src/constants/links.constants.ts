2ba516c50bfd2e6925112f20765656cce8e96ba3
Benjamin Guedj <bguedj.freelance@gmail.com>
2023-03-20 15:28:04 +0100

    feat: 🎸 Ajoute un bouton pour donner son avis (repérage TND)
    ✅ Closes: #1658

--- front/src/constants/links.constants.ts
+++ front/src/constants/links.constants.ts
@@ -7,3 +7,5 @@ export const appUrliOS =
   "itms-apps://itunes.com/app/1000-premiers-jours/id1573729958";
 export const httpsiOSAppUrl =
   "https://apps.apple.com/us/app/1000-premiers-jours/id1573729958";
+export const tndReviewUrl =
+  "https://jedonnemonavis.numerique.gouv.fr/Demarches/3589?&view-mode=formulaire-avis&nd_source=button&key=8681b4521eb48f91024faeb86f3d53f4";
