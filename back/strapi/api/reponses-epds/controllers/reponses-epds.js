f1d599bb3d58d50c5f5222056be2494f455a764c
mbarry-ippon <mbarry@ippon.fr>
2023-03-29 19:19:12 +0200

    feat: ajout champs du temps passer sur le questionnaire EPDS (#1682)

--- back/strapi/api/reponses-epds/controllers/reponses-epds.js
+++ back/strapi/api/reponses-epds/controllers/reponses-epds.js
@@ -1,9 +1,9 @@
-const contact = async () => { };
-const contactConfirmed = async () => { };
-const partage = async () => { };
-const createReponsesEpdsWidget = async () => { };
-const partagePourSoiMeme = async () => { };
-const partageEntourage = async () => { };
+const contact = async () => {};
+const contactConfirmed = async () => {};
+const partage = async () => {};
+const createReponsesEpdsWidget = async () => {};
+const partagePourSoiMeme = async () => {};
+const partageEntourage = async () => {};
 
 module.exports = {
   contact,
