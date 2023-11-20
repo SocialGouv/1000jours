4b74272b1384654235fbbc6c75200e0830961496
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-11-21 00:28:35 +0100

    fix: add CNPG prod cluster (#1729)
    * fix: add CNPG prod cluster
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * Update values.yaml
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- back/strapi/config/database.js
+++ back/strapi/config/database.js
@@ -9,10 +9,17 @@ module.exports = ({ env }) => ({
         host: env("DATABASE_HOST", "postgres"),
         password: env("DATABASE_PASSWORD", "strapi"),
         port: env.int("DATABASE_PORT", 5432),
-        ssl: env.bool("DATABASE_SSL", false),
+        ssl: getSslConfig(env),
         username: env("DATABASE_USERNAME", "strapi"),
       },
     },
   },
   defaultConnection: "default",
 });
+
+function getSslConfig(env) {
+  if (env.bool("DATABASE_SSL", false)) {
+    return { rejectUnauthorized: false }; // For self-signed certificates
+  }
+  return false;
+}
