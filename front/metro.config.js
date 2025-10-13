d430bc787203aa33b955ba7bf219ae1d5f296f7b
Benjamin <bguedj.freelance@gmail.com>
2025-10-13 16:24:14 +0200

    fix: build ios after migration of Expo SDK 53

--- front/metro.config.js
+++ front/metro.config.js
@@ -1,56 +1,18 @@
-// /* eslint-disable @typescript-eslint/no-unsafe-return */
-// const { getDefaultConfig } = require("@expo/metro-config");
-
-// module.exports = (async () => {
-//   const config = await getDefaultConfig(__dirname);
-//   const { transformer, resolver } = config;
-
-//   config.transformer = {
-//     ...transformer,
-//     babelTransformerPath: require.resolve("react-native-svg-transformer"),
-//   };
-
-//   config.resolver = {
-//     ...resolver,
-//     assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-//     sourceExts: [...resolver.sourceExts, "svg", "cjs"],
-//   };
-
-//   return config;
-// })();
-
-// /* eslint-disable @typescript-eslint/no-unsafe-return */
-// const { getDefaultConfig } = require("@expo/metro-config");
-
-// // Récupère la config par défaut Expo
-// const config = getDefaultConfig(__dirname);
-
-// // Personnalisation du transformer pour les SVG
-// config.transformer.babelTransformerPath = require.resolve(
-//   "react-native-svg-transformer"
-// );
-
-// // Ajuste les extensions
-// config.resolver.assetExts = config.resolver.assetExts.filter(
-//   (ext) => ext !== "svg"
-// );
-// config.resolver.sourceExts.push("svg", "cjs");
-
-// module.exports = config;
-
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 const { getDefaultConfig } = require("expo/metro-config");
+const { mergeConfig } = require("metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const defaultConfig = getDefaultConfig(__dirname);
+const baseConfig = getDefaultConfig(__dirname);
 
-defaultConfig.transformer.babelTransformerPath = require.resolve(
-  "react-native-svg-transformer"
-);
+const customConfig = {
+  transformer: {
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  },
+  resolver: {
+    assetExts: baseConfig.resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...baseConfig.resolver.sourceExts, "svg", "cjs"],
+  },
+};
 
-defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(
-  (ext) => ext !== "svg"
-);
-defaultConfig.resolver.sourceExts.push("svg", "cjs");
-
-module.exports = defaultConfig;
+module.exports = mergeConfig(baseConfig, customConfig);
